4be04292566bbaaa10199e204430a9b0086f7c6e
cwp <wenping.cai@rock-chips.com>
2024-02-01 10:03:23 +0800

    RVCAM release version: v1.1.0
    commit info:
    8534ce0 hal: evs: support aidl for Android14
    22c5609 Merge "hal: provider: fix rga issue for taking picture" into
    main
    
    Change-Id: I6c30bb0bab172e115332b719634d7b477b5d1c42
    Signed-off-by: cwp <wenping.cai@rock-chips.com>

--- rvcam_config.mk
+++ rvcam_config.mk
@@ -5,10 +5,37 @@
 SOONG_CONFIG_NAMESPACES += rvcam
 SOONG_CONFIG_rvcam += \
 	soc \
-	board
+	board \
+	has_hal3 \
+	has_evs \
+	has_vhal \
+	autcam_hal_version \
+	evs_hal_version \
+	vhal_hal_version
 
-SOONG_CONFIG_rvcam_soc := rk3588
-SOONG_CONFIG_rvcam_board := evb_v21
+SOONG_CONFIG_rvcam_soc ?= rk3588
+SOONG_CONFIG_rvcam_board ?= evb_v22
+SOONG_CONFIG_rvcam_has_hal3 ?= true
+SOONG_CONFIG_rvcam_has_evs ?= true
+SOONG_CONFIG_rvcam_has_vhal ?= true
 
+
+ifeq (1, $(strip $(shell expr $(PLATFORM_VERSION) \>= 14)))
+  SOONG_CONFIG_rvcam_autcam_hal_version ?= aidl_V1
+  SOONG_CONFIG_rvcam_evs_hal_version ?= aidl_V1
+  SOONG_CONFIG_rvcam_vhal_hal_version ?= aidl_V1
+else
+  SOONG_CONFIG_rvcam_autcam_hal_version ?= hidl_2_4
+  SOONG_CONFIG_rvcam_evs_hal_version ?= hidl_1_1
+  SOONG_CONFIG_rvcam_vhal_hal_version ?= hidl_2_0
+endif
+
+ifeq (1, $(strip $(shell expr $(PLATFORM_VERSION) \= 14)))
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-	hardware/rockchip/rvcam/hal/fcm/rvcam_compatibility_matrix.xml
+	hardware/rockchip/rvcam/hal/fcm/rvcam_compatibility_matrix_u.xml
+else
+ifeq (1, $(strip $(shell expr $(PLATFORM_VERSION) \= 12)))
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+	hardware/rockchip/rvcam/hal/fcm/rvcam_compatibility_matrix_s.xml
+endif
+endif
